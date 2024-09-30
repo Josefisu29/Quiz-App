bed531d666e0ca65de7f62e14ca2cd3915f9b2ce
Josefisu29 <151913443+Josefisu29@users.noreply.github.com>
2024-09-30 11:01:07 -0700

    function start

--- Qiuz.js
+++ Qiuz.js
@@ -25,3 +25,18 @@ nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
   setNextQuestion();
 });
+// Function to start the quiz
+function startGame() {
+  score = 0; // Reset score at the start of each game
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+// Function to set the next question
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
